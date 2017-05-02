ab098b4102dd50226e787a745b3441c45a51a213
WalkerPen <763436631@qq.com>
2017-05-02 23:19:07 +0800

    adapter

--- app/src/main/java/com/pen/imagepreview/MainActivity.java
+++ app/src/main/java/com/pen/imagepreview/MainActivity.java
@@ -1,13 +1,47 @@
 package com.pen.imagepreview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.pen.imagepreview.libs.ImagePreviewActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView mImage;
+    private ArrayList<String> mImages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mImages.add("http://192.168.1.109:8080/pen/biting.jpg");
+        mImages.add("http://192.168.1.109:8080/pen/biting2.jpg");
+        mImages.add("http://192.168.1.109:8080/pen/biting3.jpg");
+
+        mImage = (ImageView) findViewById(R.id.image);
+        mImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.image:
+                Intent intent = new Intent(this, ImagePreviewActivity.class);
+                intent.putStringArrayListExtra(ImagePreviewActivity.KEY_IMAGE_ARRAY, mImages);
+                intent.putExtra(ImagePreviewActivity.KEY_INDEX, 0);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                break;
+        }
     }
 }
--- libs/src/main/java/com/pen/imagepreview/libs/ImagePreviewActivity.java
+++ libs/src/main/java/com/pen/imagepreview/libs/ImagePreviewActivity.java
@@ -1,22 +1,78 @@
 package com.pen.imagepreview.libs;
 
+import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.pen.imagepreview.libs.circleindicator.CircleIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImagePreviewActivity extends AppCompatActivity {
 
+    public static final String KEY_IMAGE_ARRAY = "key_image_array";
+    public static final String KEY_INDEX = "key_index";
+
     private ViewPager mViewPager;
+    private ArrayList<String> mImages;
+    private CircleIndicator mIndicator;
+    private int mIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_preview);
 
+        initData();
         initView();
+    }
+
+    private void initData() {
+        mImages = getIntent().getStringArrayListExtra(KEY_IMAGE_ARRAY);
+        mIndex = getIntent().getIntExtra(KEY_INDEX, 0);
     }
 
     private void initView() {
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mIndicator = (CircleIndicator) findViewById(R.id.indicator);
+        MyPagerAdapter adapter = new MyPagerAdapter();
+        mViewPager.setAdapter(adapter);
+        mIndicator.setViewPager(mViewPager);
+
+    }
+
+    private class MyPagerAdapter extends PagerAdapter{
+        @Override
+        public int getCount() {
+            return mImages.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = new ImageView(container.getContext());
+            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            Glide.with(ImagePreviewActivity.this)
+                    .load(R.drawable.biting)
+                    .into(imageView);
+            container.addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 }
